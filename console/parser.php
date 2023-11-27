b87db744d42c723f46ae0b476723a0a1c1920e83
Лазарев Сергей Юрьевич <cr_ovr_web9@efko.ru>
2023-11-27 21:49:59 +0300

    fix - парсера фильмов

--- console/parser.php
+++ console/parser.php
@@ -1,90 +1,89 @@
 <?php 
 require_once('../require/db.php');
 
-$page = 0;
-while(true){
-    $page++;
+// 98c041f7-28d2-4412-880a-fe2621e3b557
+// f5bd3a8d-6669-4038-8bad-54ad15b68b11
 
-    echo $page;
+$orders = ['NUM_VOTE', 'YEAR', 'RATING'];
+$dates = ['1998', '1999'];
 
-        $url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films?order=RATING&type=FILM&ratingFrom=0&ratingTo=10&yearFrom=2013&yearTo=2013&page='.$page;
-        $headers = array(
-            'accept: application/json',
-            'X-API-KEY: b917d229-5d11-4bb2-902b-bdd90ba2d7f4'
-        );
-    
-        // Создаем cURL-сессию
-        $ch = curl_init();
-        // Устанавливаем URL-адрес и другие параметры запроса
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // Выполняем запрос
-        $response = curl_exec($ch);
-        $response = json_decode($response);
+for ($page=1; $page < 6; $page++) { 
+    foreach($orders as $order){
+        foreach($dates as $date){
 
-        // Закрываем cURL-сессию
-        curl_close($ch);
-    
-        // Возвращаем ответ
-    
-    // Пример использования API-запроса
-    $movies = $response;
-
-    if(empty($movies)){
-        echo "Скрипт завершился";
-        break;
-    }
-
-    // print_r('<pre>');
-    foreach($movies as $movie){
-        if(is_array($movie)){
-            foreach($movie as $movieInfo){
-                
-                echo $movieInfo->nameRu." \n";
-                $query = mysqli_query($db, "SELECT * FROM `movies` WHERE `kinopoiskId` = '$movieInfo->kinopoiskId'");
-                $result = mysqli_fetch_array($query);
-                if(empty($result['kinopoiskId'])){
-                    if($movieInfo->nameRu){
-                        echo "Название: ".$movieInfo->nameRu."\n";
-                        // echo "kinopoiskId: ".$movieInfo->kinopoiskId."<br>";
-                        // echo "year: ".$movieInfo->year."<br>";
-                        // echo "type: ".$movieInfo->type."<br>";
-                        // echo "posterUrl: ".$movieInfo->posterUrl."<br>";
-                        // echo "posterUrlPreview: ".$movieInfo->posterUrlPreview."<br><br>";
-    
-                        $movieValue = mysqli_query($db, "INSERT INTO `movies` 
-                        (`nameRu`, `kinopoiskId`, `year`, `type`, `posterUrl`, `posterUrlPreview`) VALUES 
-                        ('$movieInfo->nameRu', '$movieInfo->kinopoiskId', '$movieInfo->year', '$movieInfo->type', '$movieInfo->posterUrl', '$movieInfo->posterUrlPreview')"); 
-    
-                        foreach($movieInfo->countries as $country){
-                            // echo "Страны: ".$country->country.", ";
-                        
-                            $countryValue = mysqli_query($db, "INSERT INTO `countries` 
-                            (`kinopoiskId`, `country`) VALUES 
-                            ('$movieInfo->kinopoiskId', '$country->country')"); 
+            // echo $page." - ".$date." - ".$order;
+            $url = 'https://kinopoiskapiunofficial.tech/api/v2.2/films?order='.$order.'&type=FILM&ratingFrom=0&ratingTo=10&yearFrom='.$date.'&yearTo='.$date.'&page='.$page;
+            $headers = array(
+                'accept: application/json',
+                'X-API-KEY: f5bd3a8d-6669-4038-8bad-54ad15b68b11'
+            );
+        
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            // Выполняем запрос
+            $response = curl_exec($ch);
+            $response = json_decode($response);
+            curl_close($ch);
+        
             
-                        }
-    
-    
-                        foreach($movieInfo->genres as $genre){
-                            // echo "Жанры: ".$genre->genre.", ";
+            $movies = $response;
+        
+            if(empty($movies)){
+                echo "Скрипт завершился";
+                break;
+            }
+        
+            // print_r('<pre>');
+            foreach($movies as $movie){
+                if(is_array($movie)){
+                    foreach($movie as $movieInfo){
                         
-                            $countryValue = mysqli_query($db, "INSERT INTO `genres` 
-                            (`kinopoiskId`, `genre`) VALUES 
-                            ('$movieInfo->kinopoiskId', '$genre->genre')");
+                        echo $movieInfo->nameRu." \n";
+                        $query = mysqli_query($db, "SELECT * FROM `movies` WHERE `kinopoiskId` = '$movieInfo->kinopoiskId'");
+                        $result = mysqli_fetch_array($query);
+                        if(empty($result['kinopoiskId'])){
+                            if($movieInfo->nameRu){
+                                echo "Название: ".$movieInfo->nameRu."\n";
+                                // echo "kinopoiskId: ".$movieInfo->kinopoiskId."<br>";
+                                // echo "year: ".$movieInfo->year."<br>";
+                                // echo "type: ".$movieInfo->type."<br>";
+                                // echo "posterUrl: ".$movieInfo->posterUrl."<br>";
+                                // echo "posterUrlPreview: ".$movieInfo->posterUrlPreview."<br><br>";
+                                $nameRu = mysqli_real_escape_string($db, $movieInfo->nameRu);
+                                $movieValue = mysqli_query($db, "INSERT INTO `movies` 
+                                (`nameRu`, `kinopoiskId`, `year`, `type`, `posterUrl`, `posterUrlPreview`, `ratingKinopoisk`) VALUES 
+                                ('$nameRu', '$movieInfo->kinopoiskId', '$movieInfo->year', '$movieInfo->type', '$movieInfo->posterUrl', '$movieInfo->posterUrlPreview', '$movieInfo->ratingKinopoisk')"); 
+            
+                                foreach($movieInfo->countries as $country){
+                                    // echo "Страны: ".$country->country.", ";
+                                
+                                    $countryValue = mysqli_query($db, "INSERT INTO `countries` 
+                                    (`kinopoiskId`, `country`) VALUES 
+                                    ('$movieInfo->kinopoiskId', '$country->country')"); 
+                    
+                                }
+            
+            
+                                foreach($movieInfo->genres as $genre){
+                                    // echo "Жанры: ".$genre->genre.", ";
+                                
+                                    $countryValue = mysqli_query($db, "INSERT INTO `genres` 
+                                    (`kinopoiskId`, `genre`) VALUES 
+                                    ('$movieInfo->kinopoiskId', '$genre->genre')");
+                                }
+                                echo "\n";
+            
+                                // echo "<br>";
+                                // echo "<br>";
+            
+                            }
                         }
-                        echo "\n";
-    
-                        // echo "<br>";
-                        // echo "<br>";
-    
                     }
                 }
             }
+            sleep(1);
         }
     }
-
-    sleep(5);
-
 }
