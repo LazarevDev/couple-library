a78bf8343beab4b76c6516974a68f785375dc47b
Лазарев Сергей Юрьевич <cr_ovr_web9@efko.ru>
2023-12-01 15:09:36 +0300

    Стили для главной и страницы с фильмами

--- template/film.php
+++ template/film.php
@@ -1,6 +1,6 @@
 <?php 
 $kinopoiskId = $paramTwo;
-
+$titie = "sad";
 $queryMovie = mysqli_query($db, "SELECT * FROM `movies` WHERE `kinopoiskId` = $kinopoiskId");
 $resultMovie = mysqli_fetch_array($queryMovie);
 
@@ -11,47 +11,61 @@ if(!$resultMovie){
 $queryInfo = mysqli_query($db, "SELECT * FROM `movie-information` WHERE `kinopoiskId` = $kinopoiskId");
 $resultInfo = mysqli_fetch_array($queryInfo);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<section>
-    <div class="sectionContainer">
-        <div class="cover">
-            <div class="coverMask">
-                <a href="../films" class="back">
-                    <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.00001 17.75C8.80095 17.7509 8.6099 17.6716 8.47001 17.53L0.470011 9.52997C0.177558 9.23715 0.177558 8.76279 0.470011 8.46997L8.47001 0.469969C8.76552 0.19461 9.22602 0.202735 9.51163 0.488347C9.79725 0.773959 9.80537 1.23446 9.53001 1.52997L2.06001 8.99997L9.53001 16.47C9.82246 16.7628 9.82246 17.2372 9.53001 17.53C9.39013 17.6716 9.19908 17.7509 9.00001 17.75Z" fill="white"/>
-                    </svg>
-                </a>
+    <?php foreach($cssAccess as $css): ?>
+        <link rel="stylesheet" href="<?=$css?>">
+    <?php endforeach; ?>
+    
+    <title><?=$titie?></title>
+</head>
+<body>
+    <section>
+        <div class="sectionContainer">
+            <div class="cover">
+                <div class="coverMask">
+                    <a href="../films" class="back">
+                        <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.00001 17.75C8.80095 17.7509 8.6099 17.6716 8.47001 17.53L0.470011 9.52997C0.177558 9.23715 0.177558 8.76279 0.470011 8.46997L8.47001 0.469969C8.76552 0.19461 9.22602 0.202735 9.51163 0.488347C9.79725 0.773959 9.80537 1.23446 9.53001 1.52997L2.06001 8.99997L9.53001 16.47C9.82246 16.7628 9.82246 17.2372 9.53001 17.53C9.39013 17.6716 9.19908 17.7509 9.00001 17.75Z" fill="white"/>
+                        </svg>
+                    </a>
 
-                <div class="text">
-                    <h2><?=$resultMovie['nameRu']?></h2>
-                    <p>
-                        <?php
-                            $queryGenres = mysqli_query($db, "SELECT * FROM `genres` WHERE `kinopoiskId` = '$kinopoiskId'");
-                            while($rowGenres = mysqli_fetch_array($queryGenres)){
-                                echo $rowGenres['genre'].", ";
-                            }
-                        ?>
-                    </p>
-                    <span><?=$resultMovie['year']." г."?></span>
+                    <div class="text">
+                        <h2><?=$resultMovie['nameRu']?></h2>
+                        <p>
+                            <?php
+                                $queryGenres = mysqli_query($db, "SELECT * FROM `genres` WHERE `kinopoiskId` = '$kinopoiskId'");
+                                while($rowGenres = mysqli_fetch_array($queryGenres)){
+                                    echo $rowGenres['genre'].", ";
+                                }
+                            ?>
+                        </p>
+                        <span><?=$resultMovie['year']." г."?></span>
 
-                    <a href="" class="btnMovie">Будем смотреть</a>
-                    <a href="" class="btnMovieBefore">Смотреть бесплатно</a>
-                
-                    <div class="hr"></div>
+                        <a href="" class="btnMovie">Будем смотреть</a>
+                        <a href="" class="btnMovieBefore">Смотреть бесплатно</a>
+                    
+                        <div class="hr"></div>
+                    </div>
+                </div>
+
+                <img src="<?=$resultMovie['posterUrl']?>" alt="">
+            </div>
+
+            <div class="container">
+                <div class="content">
+                    <h2>Описание</h2>
+
+                    <p><?=$resultInfo['description'];?></p>
+
+                    <h2>Трейлер</h2>
                 </div>
             </div>
-
-            <img src="<?=$resultMovie['posterUrl']?>" alt="">
         </div>
-
-        <div class="container">
-            <div class="content">
-                <h2>Описание</h2>
-
-                <p><?=$resultInfo['description'];?></p>
-
-                <h2>Трейлер</h2>
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
+</body>
+</html>
--- template/films.php
+++ template/films.php
@@ -1,25 +1,77 @@
 <?php 
-echo "asd";
-?>
-<section>
-    <div class="container">
-        <div class="movies">
-            <?php 
-                $query = mysqli_query($db, "SELECT * FROM `movies` ORDER BY `year` DESC LIMIT 20");
-                while($row = mysqli_fetch_array($query)){ ?>
-                <!-- <a href="https://flicksbar.fun/film/<?=$row['kinopoiskId']?>" class="movieItem"> -->
-                <a href="film/<?=$row['kinopoiskId']?>" class="movieItem">
-                    <div class="moviePreview">
-                        <img src="<?=$row['posterUrlPreview'];?>" alt="">
-                    </div>
+$titie = "Популярные фильмы";
 
-                    <div class="movieInf">
-                        <h2><?=$row['nameRu'];?></h2>
-                        <p><?php echo $row['year']." - ".$row['ratingKinopoisk'];?></p>
-                        </div>
-                        </a>
-                <?php }
-            ?>
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php foreach($cssAccess as $css): ?>
+        <link rel="stylesheet" href="<?=$css?>">
+    <?php endforeach; ?>
+    
+    <title><?=$titie?></title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="headerTitle"><h2>Привет, Сергей</h2></div>
         </div>
-    </div>
-</section>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="partnersContainer">
+                <a href="#" class="partnerBlock">
+                    <div class="partnerImg"></div>
+
+                    <div class="partnerText">
+                        <h2>Имя Фамилия</h2>
+                        <p>Перейти в профиль</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="titleSection">
+                <h2>Популярное</h2>
+            </div>
+            
+            <div class="movies">
+                <?php 
+                    $query = mysqli_query($db, "SELECT * FROM `movies` ORDER BY `year` DESC LIMIT 20");
+                    while($row = mysqli_fetch_array($query)){ 
+                        $kinopoiskId = $row['kinopoiskId'];
+                        ?>
+                    <!-- <a href="https://flicksbar.fun/film/<?=$row['kinopoiskId']?>" class="movieItem"> -->
+                    <div class="movieItem">
+                        <a href="film/<?=$row['kinopoiskId']?>" class="moviePreview">
+                            <span class="rating"><?=$row['ratingKinopoisk']?></span>
+    
+                            <div class="genreMask"><p>
+                                <?php 
+                                $queryGenres = mysqli_query($db, "SELECT * FROM `genres` WHERE `kinopoiskId` = '$kinopoiskId'"); 
+                                while($rowGenre = mysqli_fetch_array($queryGenres)){
+                                    echo $rowGenre['genre'].", ";
+                                }
+                                ?></p>
+                            </div>
+
+                            <img src="<?=$row['posterUrlPreview'];?>" alt="">
+                        </a>
+
+                        <div class="movieInfo">
+                            <a href="" class="btnView">Будем смотреть</a>
+                            <h2><?=$row['nameRu']?></h2>
+                            <p><?=$row['year']?></p>
+                        </div>
+                    </div>
+                    <?php }
+                ?>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
