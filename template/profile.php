3387a0e20ee5750bad40c9f6ed24ac02a0da7079
Лазарев Сергей Юрьевич <cr_ovr_web9@efko.ru>
2023-12-03 20:56:54 +0300

    стили для профиля

--- template/profile.php
+++ template/profile.php
@@ -101,7 +101,7 @@ if(!empty($paramTwo)){
                         var id = document.getElementById("btn").getAttribute("data-id");
                         var xhr = new XMLHttpRequest();
                     
-                        xhr.open("POST", "../template/test.php", true);
+                        xhr.open("POST", "../template/subscribe.php", true);
                         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
                         xhr.onreadystatechange = function() {
                             if (xhr.readyState === 4 && xhr.status === 200) {
